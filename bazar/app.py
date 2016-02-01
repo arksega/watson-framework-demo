46189ecc6e047bfb7a6d5e20327da245602c86ac
Sergio Gutierrez <ark.sega@gmail.com>
2016-02-01 01:46:33 -0600

    Adding decorator to check session status (logged)

--- bazar/app.py
+++ bazar/app.py
@@ -2,4 +2,15 @@
 from watson.framework import applications
 from bazar.config import config
 
+def logged(func):
+    def inner(self, *args, **kargs):
+        print('Decorated', self)
+        print(self.request.session['salt'])
+        if not self.request.session['salt']:
+            self.redirect('/login')
+        return func(self, *args, **kargs)
+    return inner
+
 application = applications.Http(config)
+
+# vim: sw=4 sts=4 et si
--- bazar/controllers/index.py
+++ bazar/controllers/index.py
@@ -2,8 +2,13 @@
 from watson import framework
 from watson.framework import controllers
 
+from bazar.app import logged
 
 class Index(controllers.Rest):
+
+    @logged
     def GET(self):
         print('Control!')
         return 'Welcome to Watson v{0}!'.format(framework.__version__)
+
+# vim: sw=4 sts=4 et si
